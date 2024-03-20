0548bf02b5543164c143375a70fa7ccdd0e02d33
ThuanNV08 <tvan77630@gmail.com>
2024-03-21 02:44:58 +0700

    add trang cam_on

--- view/footer.php
+++ view/footer.php
@@ -98,7 +98,7 @@
         <div class="container">
             <div class="row">
                 <div class="col text-center">
-                    <p class="footer-copyright">© 2022 Jadusona. Made with <i class="fa fa-heart heart-icon"></i> By <a target="_blank" href="https://hasthemes.com/">HasThemes</a></p>
+                    <p class="footer-copyright">© 2024 FPOLY Hà Nội. Made with <i class="fa fa-heart heart-icon"></i> By <a target="_blank" href="https://hasthemes.com/">Team 12</a></p>
                 </div>
             </div>
         </div>
